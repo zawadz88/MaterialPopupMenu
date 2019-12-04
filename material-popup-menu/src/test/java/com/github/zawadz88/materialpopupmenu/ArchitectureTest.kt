0fdbe106d67ae36b3ac803d26e7c1f232dc5a414
Piotr Zawadzki <Piotr.Zawadzki@stepstone.com>
2019-12-04 20:19:22 +0100

    Added ArchUnit for enforcing `internal` package components do not leak into API. Added MaterialComponents in the sample app.

---
+++ material-popup-menu/src/test/java/com/github/zawadz88/materialpopupmenu/ArchitectureTest.kt
@@ -0,0 +1,22 @@
+package com.github.zawadz88.materialpopupmenu
+
+import androidx.appcompat.widget.MaterialRecyclerViewPopupWindow
+import com.github.zawadz88.materialpopupmenu.internal.PopupMenuAdapter
+import com.tngtech.archunit.junit.AnalyzeClasses
+import com.tngtech.archunit.junit.ArchTest
+import com.tngtech.archunit.junit.ArchUnitRunner
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes
+import org.junit.runner.RunWith
+
+@RunWith(ArchUnitRunner::class)
+@AnalyzeClasses(packages = ["com.github.zawadz88.materialpopupmenu"])
+class ArchitectureTest {
+
+    @ArchTest
+    val internal_api_restriction = classes()
+        .that().resideInAPackage("com.github.zawadz88.materialpopupmenu.internal")
+        .should()
+        .onlyBeAccessed().byClassesThat().resideInAPackage("com.github.zawadz88.materialpopupmenu.internal")
+        .orShould()
+        .onlyBeAccessed().byClassesThat().belongToAnyOf(MaterialPopupMenu::class.java, MaterialRecyclerViewPopupWindow::class.java, PopupMenuAdapter::class.java)
+}
